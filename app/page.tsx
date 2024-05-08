6ae403dc89cf28622ee2c9d4051033173c6b43ff
Jannick Pedersen <jannickpedersen@live.dk>
2024-05-08 23:17:45 +0200

    setup a finer mask text reveal

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { MottoText } from "@/components/MottoText";
+
+
+export default function Home() {
+
+  return (
+    <main>
+      <MottoText />
+    </main>
+  );
+  
+}
---
+++ components/MottoText.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import styles from "./mottotext.module.scss";
+import { motion } from "framer-motion";
+import useMousePosition from "@/utils/useMousePosition";
+
+export const MottoText = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 400 : 40;
+
+  return (
+    <div className={styles.main}>
+      <motion.div
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "easeOut", duration: 0.5 }}
+      >
+        <p
+          className="text-zinc-800 text-center"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          You gotta hover to discover whats undercover
+        </p>
+      </motion.div>
+
+      <div className={styles.body}>
+        <div className="text-zinc-500 text-center uppercase text-[7rem]">
+          <p className="text-zinc-200">Jannick Pedersen</p>
+        </div>
+      </div>
+    </div>
+  );
+};
